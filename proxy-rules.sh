4e70e6af67dfc6d1f13af90fd54a50accbb2008d
xsmzdd <111579783+xsmzdd@users.noreply.github.com>
2025-02-02 13:01:15 +0800

    proxy-rules.sh

--- proxy-rules.sh
+++ proxy-rules.sh
@@ -18,6 +18,6 @@ iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
 # 重定向 TCP 流量到 Redsocks 端口
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
 
-# 应用规则到 OUTPUT 和 PREROUTING 链（替换 enp0s6 为实际网卡名）
+# 应用规则到 OUTPUT 和 PREROUTING 链（替换 eth0 为实际网卡名）
 iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
-iptables -t nat -A PREROUTING -i enp0s6 -p tcp -j REDSOCKS
+iptables -t nat -A PREROUTING -i eth0 -p tcp -j REDSOCKS
